1127480ecfe32cdfcf603d7fd5f4b7295cd2cd69
Sheyvie <sheilayvi@gmail.com>
2023-09-15 11:32:58 +0300

    First Commit

---
+++ BookManagementSystem/Models/BookDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookManagementSystem.Models
+{
+    public class BookDto
+    {
+
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public List<Author> Authors { get; set; } =new List<Author>();
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string AuthName { get; set; } = string.Empty;
+
+    }
+}
+
